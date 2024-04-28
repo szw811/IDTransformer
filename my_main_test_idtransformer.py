78b94f648c6c5eef5c5c2ae98e098d2f5e1ffcc8
szw <48083424+szw811@users.noreply.github.com>
2024-04-28 08:21:38 +0800

    Add files via upload

---
+++ my_main_test_idtransformer.py
@@ -0,0 +1,174 @@
+import os.path
+import logging
+import re
+
+import numpy as np
+from collections import OrderedDict
+
+import torch
+
+from utils import utils_logger
+from utils import utils_image as util
+from utils import utils_model
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+def main():
+
+    # ----------------------------------------
+    # Preparation
+    # ----------------------------------------
+
+    model_name = 'idtransformer_15'
+    testset_name = 'IR700_test'                # test set,  'set5' | 'srbsd68'
+    need_degradation = True              # default: True
+    x8 = False                           # default: False, x8 to boost performance, default: False
+    show_img = False                     # default: False
+
+    noise_level_img = 15  # fixed: 0, noise level for LR image
+
+    sf = 1
+    task_current = 'dn'
+    n_channels = 1            # fixed
+    border = sf if task_current == 'sr' else 0
+    model_pool = 'model_zoo'  # fixed
+    testsets = 'testsets'     # fixed
+    results = 'results'       # fixed
+    result_name = testset_name + '_' + model_name
+    model_path = os.path.join(model_pool, model_name+'.pth')
+
+    # ----------------------------------------
+    # L_path, E_path, H_path
+    # ----------------------------------------
+
+    L_path = os.path.join(testsets, testset_name) # L_path, for Low-quality images
+    H_path = L_path                               # H_path, for High-quality images
+    E_path = os.path.join(results, result_name)   # E_path, for Estimated images
+    util.mkdir(E_path)
+
+    if H_path == L_path:
+        need_degradation = True
+    logger_name = result_name
+    utils_logger.logger_info(logger_name, log_path=os.path.join(E_path, logger_name+'.log'))
+    logger = logging.getLogger(logger_name)
+
+    need_H = True if H_path is not None else False
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # ----------------------------------------
+    # load model
+    # ----------------------------------------
+    from models.network_idtransformer import IDTransformer as net
+
+    model = net(inp_channels=n_channels, out_channels=n_channels, dim=48, num_blocks=[4, 6, 6, 8],
+                img_range=1., num_refinement_blocks=4, heads=[1, 2, 4, 8], ffn_expansion_factor=2.66,
+                bias=False, LayerNorm_type='WithBias', dual_pixel_task=False)
+    param_key_g = 'params'
+
+    pretrained_model = torch.load(model_path)
+    model.load_state_dict(pretrained_model[param_key_g] if param_key_g in pretrained_model.keys() else pretrained_model,
+                          strict=True)
+
+    # model.load_state_dict(torch.load(model_path), strict=True)
+
+    model.eval()
+    for k, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
+    logger.info('Model path: {:s}'.format(model_path))
+    number_parameters = sum(map(lambda x: x.numel(), model.parameters()))
+    logger.info('Params number: {}'.format(number_parameters))
+
+    test_results = OrderedDict()
+    test_results['psnr'] = []
+    test_results['ssim'] = []
+    test_results['psnr_y'] = []
+    test_results['ssim_y'] = []
+
+    logger.info('model_name:{}, image sigma:{}'.format(model_name, noise_level_img))
+    logger.info(L_path)
+    L_paths = util.get_image_paths(L_path)
+    H_paths = util.get_image_paths(H_path) if need_H else None
+
+    for idx, img in enumerate(L_paths):
+
+        # ------------------------------------
+        # (1) img_L
+        # ------------------------------------
+
+        img_name, ext = os.path.splitext(os.path.basename(img))
+        # logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
+        img_L = util.imread_uint(img, n_channels=n_channels)
+        img_L = util.uint2single(img_L)
+        # if idx == 0:
+        #     print(img_L.shape)
+
+        # degradation process, bicubic downsampling
+        if need_degradation:
+            np.random.seed(seed=0)
+            img_L += np.random.normal(0, noise_level_img/255., img_L.shape)
+
+        util.imshow(util.single2uint(img_L), title='Image with noise level {}'.format(noise_level_img)) if show_img else None
+
+        img_L = util.single2tensor4(img_L)
+        img_L = img_L.to(device)
+
+        # ------------------------------------
+        # (2) img_E
+        # ------------------------------------
+
+        if not x8:
+            img_E = model(img_L)
+
+        else:
+            img_E = utils_model.test_mode(model, img_L, mode=3, sf=sf)
+
+        img_E = util.tensor2uint(img_E)
+
+        if need_H:
+
+            # --------------------------------
+            # (3) img_H
+            # --------------------------------
+
+            img_H = util.imread_uint(H_paths[idx], n_channels=n_channels)
+            img_H = img_H.squeeze()
+
+            # --------------------------------
+            # PSNR and SSIM
+            # --------------------------------
+
+            psnr = util.calculate_psnr(img_E, img_H, border=border)
+            ssim = util.calculate_ssim(img_E, img_H, border=border)
+            test_results['psnr'].append(psnr)
+            test_results['ssim'].append(ssim)
+            logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
+            util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if show_img else None
+
+            # 在裁剪出的y通道上计算psnr和ssim
+            if np.ndim(img_H) == 3:  # RGB image
+                img_E_y = util.rgb2ycbcr(img_E, only_y=True)
+                img_H_y = util.rgb2ycbcr(img_H, only_y=True)
+                psnr_y = util.calculate_psnr(img_E_y, img_H_y, border=border)
+                ssim_y = util.calculate_ssim(img_E_y, img_H_y, border=border)
+                test_results['psnr_y'].append(psnr_y)
+                test_results['ssim_y'].append(ssim_y)
+
+        # ------------------------------------
+        # save results
+        # ------------------------------------
+
+        util.imsave(img_E, os.path.join(E_path, img_name+'.png'))
+
+    if need_H:
+        ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
+        ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
+        logger.info('Average PSNR/SSIM(RGB) - {} - x{} --PSNR: {:.2f} dB; SSIM: {:.4f}'.format(result_name, sf, ave_psnr, ave_ssim))
+        if np.ndim(img_H) == 3:
+            ave_psnr_y = sum(test_results['psnr_y']) / len(test_results['psnr_y'])
+            ave_ssim_y = sum(test_results['ssim_y']) / len(test_results['ssim_y'])
+            logger.info('Average PSNR/SSIM( Y ) - {} - x{} - PSNR: {:.2f} dB; SSIM: {:.4f}'.format(result_name, sf, ave_psnr_y, ave_ssim_y))
+
+if __name__ == '__main__':
+    main()
